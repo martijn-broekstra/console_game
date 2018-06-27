53b5ee1fa9be51ba164bf48f46e68f97bbcf3da5
Martijn Broekstra <martijn@about-blank.nl>
2018-06-27 16:38:17 +0200

    first commit

---
+++ CoreConsoleApp/Program.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Text;
+
+namespace CoreConsoleApp
+{
+    public class GridItem
+    {
+        public static GridItem Empty
+        {
+            get
+            {
+                return new GridItem('\0', ConsoleColor.Black);
+            }
+        }
+        public char Character { get; set; }
+        public ConsoleColor Color { get; set; }
+
+        public bool IsEmpty
+        {
+            get
+            {
+                return Character == '\0';
+            }
+        }
+
+        public bool IsBlocking
+        {
+            get
+            {
+                return Character == 'X';
+            }
+        }
+
+        public GridItem(char character, ConsoleColor color)
+        {
+            this.Character = character;
+            this.Color = color;
+        }
+    }
+
+    public class Grid
+    {
+        private GridItem[,] _grid;
+
+        public int Width
+        {
+            get
+            {
+                return _grid.GetLength(0);
+            }
+        }
+        public int Height
+        {
+            get
+            {
+                return _grid.GetLength(1);
+            }
+        }
+
+        public GridItem this[int x, int y]
+        {
+            get
+            {
+                return _grid[x, y] ?? GridItem.Empty;
+            }
+            private set
+            {
+                _grid[x, y] = value;
+            }
+        }
+
+        public Grid(int width, int height)
+        {
+            _grid = new GridItem[width, height];
+
+            // Test
+            _grid[7, 15] = new GridItem('X', ConsoleColor.Red);
+            _grid[7, 14] = new GridItem('X', ConsoleColor.Red);
+            _grid[7, 13] = new GridItem('X', ConsoleColor.Red);
+            _grid[7, 12] = new GridItem('X', ConsoleColor.Red);
+            _grid[7, 11] = new GridItem('X', ConsoleColor.Red);
+            _grid[7, 10] = new GridItem('X', ConsoleColor.Red);
+            _grid[8, 15] = new GridItem('2', ConsoleColor.Cyan);
+            _grid[9, 15] = new GridItem('3', ConsoleColor.Cyan);
+            _grid[10, 15] = new GridItem('+', ConsoleColor.Cyan);
+            _grid[11, 15] = new GridItem('5', ConsoleColor.Cyan);
+            _grid[11, 14] = new GridItem('6', ConsoleColor.Cyan);
+            _grid[11, 13] = new GridItem('7', ConsoleColor.Cyan);
+            _grid[11, 12] = new GridItem('8', ConsoleColor.Cyan);
+        }
+
+        public bool CanMove(Coordinate coordinate, Coordinate direction)
+        {
+            var nextCoordinate = coordinate + direction;
+            nextCoordinate.Wrap(this.Width, this.Height);
+
+            if(this[nextCoordinate.X, nextCoordinate.Y].IsEmpty)
+            {
+                return true;
+            }
+            else if(this[nextCoordinate.X, nextCoordinate.Y].IsBlocking)
+            {
+                return false;
+            }
+            else
+            {
+                return CanMove(nextCoordinate, direction);
+            }
+        }
+
+        private void ExecuteMove(Coordinate coordinate, Coordinate direction)
+        {
+            var nextCoordinate = coordinate + direction;
+            nextCoordinate.Wrap(this.Width, this.Height);
+
+            if (!this[nextCoordinate.X, nextCoordinate.Y].IsEmpty)
+            {
+                ExecuteMove(nextCoordinate, direction);
+            }
+
+            this[nextCoordinate.X, nextCoordinate.Y] = this[coordinate.X, coordinate.Y];
+            this[coordinate.X, coordinate.Y] = GridItem.Empty;
+        }
+
+        public void Move(Coordinate coordinate, Coordinate direction)
+        {
+            if(CanMove(coordinate, direction))
+            {
+                ExecuteMove(coordinate, direction);
+            }
+        }
+    }
+
+    public struct Coordinate
+    {
+        public static Coordinate Up
+        {
+            get
+            {
+                return new Coordinate(0, -1);
+            }
+        }
+
+        public static Coordinate Down
+        {
+            get
+            {
+                return new Coordinate(0, 1);
+            }
+        }
+
+        public static Coordinate Left
+        {
+            get
+            {
+                return new Coordinate(-1, 0);
+            }
+        }
+
+        public static Coordinate Right
+        {
+            get
+            {
+                return new Coordinate(1, 0);
+            }
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public static Coordinate operator +(Coordinate left, Coordinate right)
+        {
+            return new Coordinate { X = left.X + right.X, Y = left.Y + right.Y };
+        }
+
+        public Coordinate(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void Wrap(int width, int height)
+        {
+            X = X >= width ? 0 : X;
+            X = X < 0 ? width - 1 : X;
+
+            Y = Y >= height ? 0 : Y;
+            Y = Y < 0 ? height - 1 : Y;
+        }
+    }
+    class Program
+    {
+        public const int _xOffset = 5;
+        public const int _yOffset = 5;
+        public const int _width = 20;
+        public const int _height = 20;
+
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(_width + 2 * _xOffset, _height + 2 * _yOffset);
+            Console.CursorVisible = false;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            var color = ConsoleColor.Cyan;
+            var grid = new Grid(_width, _height);
+
+
+            var position = new Coordinate(0, 0);
+
+            while (true)
+            {
+                DrawRectangle(_xOffset - 1, _yOffset - 1, _width, _height, color);
+                DrawGrid(grid);
+                DrawChar(position.X, position.Y, 'ȯ', color);
+
+                var key = Console.ReadKey(true);
+                Coordinate? direction = null;
+                switch (key.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        direction = Coordinate.Up;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        direction = Coordinate.Down;
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        direction = Coordinate.Left;
+                        break;
+                    case ConsoleKey.RightArrow:
+                        direction = Coordinate.Right;
+                        break;
+                }
+
+
+                if (direction.HasValue && grid.CanMove(position, direction.Value))
+                {
+                    grid.Move(position, direction.Value);
+                    position += direction.Value;
+                }
+
+                position.Wrap(_width, _height);
+            }
+        }
+
+        public static void DrawGrid(Grid grid)
+        {
+            for(int i = 0; i < grid.Width; i++)
+            {
+                for(int j = 0; j < grid.Height; j++)
+                {
+                    DrawChar(i, j, grid[i, j].Character, grid[i, j].Color);
+                }
+            }
+        }
+
+        public static void DrawChar(int x, int y, char character, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.CursorTop = y + _yOffset;
+            Console.CursorLeft = x + _xOffset;
+
+            Console.Write(character);
+            Console.ResetColor();
+        }
+        public static void DrawRectangle(int x, int y, int width, int height, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+
+            Console.CursorTop = y;
+            Console.CursorLeft = x;
+
+            string line = "╔";
+            for (int i = 0; i < width; i++)
+            {
+                line += "═";
+            }
+            line += "╗";
+
+            Console.Write(line);
+
+            for (int i = 0; i < height; i++)
+            {
+                Console.CursorTop = y + i + 1;
+                Console.CursorLeft = x;
+
+                Console.Write("║");
+                Console.CursorLeft = x + 1 + width;
+                Console.Write("║");
+            }
+            Console.CursorTop = y + height + 1;
+            Console.CursorLeft = x;
+
+            line = "╚";
+            for (int i = 0; i < width; i++)
+                line += "═";
+
+            line += "╝";
+            Console.Write(line);
+
+            Console.ResetColor();
+        }
+
+    }
+}
---
+++ LevelEditor/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿using System.Windows.Forms;
+
+namespace LevelEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Controls.Clear();
+
+            this.levelWidth = new System.Windows.Forms.TextBox();
+            this.levelHeight = new System.Windows.Forms.TextBox();
+            this.resizeButton = new System.Windows.Forms.Button();
+            this.gridPanel = new Panel();
+
+            this.gridPanel.SuspendLayout();
+            this.SuspendLayout();
+
+            // 
+            // tableLayoutPanel1
+            // 
+
+            this.levelWidth.Location = new System.Drawing.Point(10, 20);
+            this.levelWidth.Width = 100;
+            this.levelWidth.TabIndex = 0;
+
+            this.levelHeight.Location = new System.Drawing.Point(120, 20);
+            this.levelHeight.Width = 100;
+            this.levelHeight.TabIndex = 0;
+
+            this.resizeButton.Location = new System.Drawing.Point(230, 20);
+            this.resizeButton.Text = "resize";
+            this.resizeButton.Click += new System.EventHandler(ClickResize);
+
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(916, 590);
+
+            this.gridPanel.Location = new System.Drawing.Point(22, 95);
+            this.gridPanel.Size = new System.Drawing.Size(871, 483);
+
+            this.Controls.Add(this.gridPanel);
+            this.Controls.Add(this.levelWidth);
+            this.Controls.Add(this.levelHeight);
+            this.Controls.Add(this.resizeButton);
+
+            this.Name = "LevelEditor";
+            this.Text = "LevelEditor";
+            this.gridPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        private TableLayoutPanel CreateTable(int width, int height)
+        {
+            var table = new TableLayoutPanel();
+            table.ColumnCount = width;
+            table.RowCount = height;
+
+            for (int i = 0; i < width; i++)
+            {
+                table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100 / width));
+                for (int j = 0; j < height; j++)
+                {
+                    var button = new System.Windows.Forms.Button();
+                    table.Controls.Add(button, i, j);
+
+                    button.Location = new System.Drawing.Point(3, 3);
+                    button.Name = $"button{i}_{j}";
+                    button.Size = new System.Drawing.Size(75, 23);
+                    button.TabIndex = 0;
+                    button.Text = $"button{i}_{j}";
+                    button.UseVisualStyleBackColor = true;
+
+                    button.Click += new System.EventHandler(ClickButton);
+                }
+            }
+            for (int j = 0; j < height; j++)
+            {
+                table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100 / height));
+            }
+
+            table.Dock = DockStyle.Fill;
+            table.Name = "levelGrid";
+            table.TabIndex = 0;
+
+            return table;
+        }
+
+        private void ClickResize(object sender, System.EventArgs e)
+        {
+            width = int.Parse(levelWidth.Text);
+            height = int.Parse(levelHeight.Text);
+
+            this.gridPanel.Controls.Clear();
+            this.gridPanel.Controls.Add(CreateTable(width, height));
+        }
+
+        private void ClickButton(object sender, System.EventArgs e)
+        {
+            var form = new GridItemPopupForm();
+
+            form.ShowDialog(this);
+        }
+
+        private int width = 10;
+        private int height = 10;
+
+        private System.Windows.Forms.Button resizeButton;
+        private System.Windows.Forms.TextBox levelWidth;
+        private System.Windows.Forms.TextBox levelHeight;
+        private System.Windows.Forms.Panel gridPanel;
+    }
+}
+
